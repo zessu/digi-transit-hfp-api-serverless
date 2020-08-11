f3e77343f0ebbfe1e2de32fd88d5c7facd118154
Wambua Makenzi <amackzie@gmail.com>
2020-08-11 15:29:44 +0300

    create basic lambdas for operations

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 import poll from './vehicle-status-poller';
 import request from './vehicle-tracking-request';
 import retreive from './vehicle-status-retrieve';
+import commonMiddleware from './commonMiddleware';
+
+const getVehicleStatus = async () => {
+  const message = 'getVehicleStatus has been called';
+  return {
+    statusCode: 200,
+    body: message,
+  };
+};
+
+const startPolling = async () => {
+  const message = 'startPolling has been called';
+  return {
+    statusCode: 200,
+    body: message,
+  };
+};
+
+const requestTracker = async () => {
+  const message = 'requestTracker has been called';
+  return {
+    statusCode: 200,
+    body: message,
+  };
+};
+
+exports.getVehicleStatus = getVehicleStatus;
+exports.startPolling = startPolling;
+exports.requestTracker = requestTracker;
