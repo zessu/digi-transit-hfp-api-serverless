c92d5ff41e5ed2018ab18d453ff44a201549b916
Wambua Makenzi <amackzie@gmail.com>
2020-08-18 17:51:55 +0300

    update lambda function to get bus info

--- vehicle-status-retrieve/index.js
+++ vehicle-status-retrieve/index.js
@@ -1,5 +1,34 @@
-"use strict";
+import { AWS } from 'aws-sdk';
+import createError from 'http-errors';
 
-module.exports.getVehicleStatus = async (event, context) => {
-  // TODO
+const ecs = new AWS.ECS();
+
+exports.getVehicleStatus = async (event) => {
+  const { id } = event.pathParameters;
+  const gatewayResource = 'hslpollerLoadBalancer-1683342120.eu-west-1.elb.amazonaws.com';
+  const requestUrl = `${gatewayResource}/request/${id}`;
+  const params = {
+    cluster: 'hslpollerCluster',
+    count: 1,
+    taskDefinition: 'hslpollerTaskDefinition',
+  };
+  try {
+    ecs.runTask(params, (err) => {
+      if (err) {
+        return new createError.InternalServerError('there was a problem starting the task');
+      }
+      return fetch(requestUrl)
+        .then((response) => {
+          const payload = response;
+          return {
+            statusCode: 200,
+            body: `${JSON.stringify(payload)}`,
+          };
+        })
+        .catch((error) => { throw new Error(error); }); // caught by the catch block
+    });
+  } catch (error) {
+    console.error(`error is ${error}`);
+    throw new createError.InternalServerError('an error has occurred processing that request');
+  }
 };
